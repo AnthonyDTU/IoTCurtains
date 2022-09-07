33eed08794931536c6e47a6897cff9ba4fc9a8a0
AnthonyDTU <s191270@student.dtu.dk>
2022-09-07 21:00:13 +0200

    initial commit
    MAUI and nanoFramework projects created

---
+++ SmartCurtains/SmartCurtains/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace SmartCurtains;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
---
+++ SmartCurtains/SmartCurtains/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace SmartCurtains;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
