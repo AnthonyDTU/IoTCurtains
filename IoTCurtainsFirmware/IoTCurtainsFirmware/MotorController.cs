81a0962df6466a3fc5eedf7e399578c35040b35f
AnthonyDTU <s191270@student.dtu.dk>
2022-09-08 12:11:16 +0200

    Downloaded NuGet packages for the firmware.
    Started implementing preliminary code to get a feel for the components and see what is already there

---
+++ IoTCurtainsFirmware/IoTCurtainsFirmware/MotorController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Device.Gpio;
+using System.Diagnostics;
+using System.Threading;
+using Iot.Device.DCMotor;
+using Iot.Device.RotaryEncoder;
+
+namespace IoTCurtainsFirmware
+{
+    internal class MotorController
+    {
+        GpioPin pwmPin;
+        GpioPin clockwiseDirectionPin;
+        QuadratureRotaryEncoder rotaryEncoder;
+
+        private int currentState;
+        private int clicksFromMin;
+        private int clicksFromMax;
+
+        private bool calibrated = false;
+        
+        public MotorController(GpioPin pwmPin, GpioPin clockwiseDirectionPin, QuadratureRotaryEncoder rotaryEncoder)
+        {
+            this.pwmPin = pwmPin;
+            this.clockwiseDirectionPin = clockwiseDirectionPin;
+            this.rotaryEncoder = rotaryEncoder;
+        }
+
+
+
+
+
+
+
+
+
+
+    }
+}
--- IoTCurtainsFirmware/IoTCurtainsFirmware/Program.cs
+++ IoTCurtainsFirmware/IoTCurtainsFirmware/Program.cs
@@ -1,20 +1,36 @@
+using Iot.Device.RotaryEncoder;
 using System;
+using System.Device.Gpio;
 using System.Diagnostics;
 using System.Threading;
+using Windows.Devices.Pwm;
+
 
 namespace IoTCurtainsFirmware
 {
     public class Program
     {
+        static int pwmPinNumber = 1;
+        static int clockwiseRotationPinNumber = 2;
+
+        static int rotaryEncoderPinA = 3;
+        static int rotaryEncoderPinB = 4;
+        static int rotaryEncoderClicksPerRotation = 10;
+
         public static void Main()
         {
-            Debug.WriteLine("Hello from nanoFramework!");
+            GpioController gpioController = new GpioController(PinNumberingScheme.Board);
+            QuadratureRotaryEncoder rotaryEncoder = new QuadratureRotaryEncoder(rotaryEncoderPinA, rotaryEncoderPinB, rotaryEncoderClicksPerRotation);
+
+            MotorController motorController = new MotorController(gpioController.OpenPin(pwmPinNumber), gpioController.OpenPin(clockwiseRotationPinNumber), rotaryEncoder);
+
+            while (true)
+            {
+                
+
+            }
 
             Thread.Sleep(Timeout.Infinite);
-
-            // Browse our samples repository: https://github.com/nanoframework/samples
-            // Check our documentation online: https://docs.nanoframework.net/
-            // Join our lively Discord community: https://discord.gg/gCyBu8T
         }
     }
 }
