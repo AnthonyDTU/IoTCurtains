9510261a3afe5f4eb7d5a8a5fcd67fa0acd093ae
AnthonyDTU <71980488+AnthonyDTU@users.noreply.github.com>
2022-09-20 21:43:56 +0200

    Downloaded to Mac

---
+++ IoTCurtains/IoTCurtains/obj/Debug/net6.0-android/android/src/crc64a991e00f2aa404aa/MainActivity.java
@@ -0,0 +1,46 @@
+package crc64a991e00f2aa404aa;
+
+
+public class MainActivity
+	extends crc6488302ad6e9e4df1a.MauiAppCompatActivity
+	implements
+		mono.android.IGCUserPeer
+{
+/** @hide */
+	public static final String __md_methods;
+	static {
+		__md_methods = 
+			"";
+		mono.android.Runtime.register ("IoTCurtains.MainActivity, IoTCurtains", MainActivity.class, __md_methods);
+	}
+
+
+	public MainActivity ()
+	{
+		super ();
+		if (getClass () == MainActivity.class)
+			mono.android.TypeManager.Activate ("IoTCurtains.MainActivity, IoTCurtains", "", this, new java.lang.Object[] {  });
+	}
+
+
+	public MainActivity (int p0)
+	{
+		super (p0);
+		if (getClass () == MainActivity.class)
+			mono.android.TypeManager.Activate ("IoTCurtains.MainActivity, IoTCurtains", "System.Int32, System.Private.CoreLib", this, new java.lang.Object[] { p0 });
+	}
+
+	private java.util.ArrayList refList;
+	public void monodroidAddReference (java.lang.Object obj)
+	{
+		if (refList == null)
+			refList = new java.util.ArrayList ();
+		refList.add (obj);
+	}
+
+	public void monodroidClearReferences ()
+	{
+		if (refList != null)
+			refList.clear ();
+	}
+}
